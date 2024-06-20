b75d3b1b5543a3e76a6ee033463ca65761c63484
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-06-20 16:47:32 -0300

    [ADD][TASK-????] Atualizando com angular 18. Sem o standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RouterOutlet } from "@angular/router";
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from "./app.component";
-
-import { SalaryModule } from "./salary/salary.module";
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    FormsModule,
-    RouterOutlet,
-    SalaryModule,
-    BrowserModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        RouterModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+    api: {
+        url: 'http://api.salary-discount-calculator.local.com'
+    },
+    debugRequest: false,
+    lang: 'pt',
+};
