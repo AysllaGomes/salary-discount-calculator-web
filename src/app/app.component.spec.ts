025d3268e2961d6b307c499e87c02acd492fb136
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-06-21 09:10:40 -0300

    [UPT][TASK-????] corrigindo alguns testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+describe('AppComponent', (): void => {
+    beforeEach(async (): Promise<void> => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', (): void => {
+        const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+        const app: AppComponent = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'salary-discount-calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('salary-discount-calculator');
-  });
+    it(`should have as title 'salary-discount-calculator'`, (): void => {
+        const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+        const app: AppComponent = fixture.componentInstance;
+        expect(app.title).toEqual('salary-discount-calculator');
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('salary-discount-calculator app is running!');
-  });
+    it('should render title', (): void => {
+        const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled: HTMLElement = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('.content span')?.textContent).toContain('salary-discount-calculator app is running!');
+    });
 });
--- src/app/salary/component/salary/salary.component.spec.ts
+++ src/app/salary/component/salary/salary.component.spec.ts
@@ -1,23 +1,43 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SalaryComponent } from './salary.component';
 
-describe('SalaryComponent', () => {
-  let component: SalaryComponent;
-  let fixture: ComponentFixture<SalaryComponent>;
+describe('SalaryComponent', (): void => {
+    let component: SalaryComponent;
+    let fixture: ComponentFixture<SalaryComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SalaryComponent]
-    })
-    .compileComponents();
+    beforeEach(async (): Promise<void> => {
+        await TestBed.configureTestingModule({
+            declarations: [SalaryComponent],
+            imports: [FlexLayoutModule],
+            schemas: [NO_ERRORS_SCHEMA]
+        }).compileComponents();
+    });
 
-    fixture = TestBed.createComponent(SalaryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach((): void => {
+        fixture = TestBed.createComponent(SalaryComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', (): void => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should render title in a h3 tag', (): void => {
+        const compiled: HTMLElement = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('h3')?.textContent).toContain('Calculadora de Salário Bruto');
+    });
+
+    it('should render app-salary-discount component', (): void => {
+        const compiled: HTMLElement = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('app-salary-discount')).toBeTruthy();
+    });
+
+    it('should render app-salary-about component', (): void => {
+        const compiled: HTMLElement = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('app-salary-about')).toBeTruthy();
+    });
 });
