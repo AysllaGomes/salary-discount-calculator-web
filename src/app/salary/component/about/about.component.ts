14269d03df9e61fcaadbef3fb753f805701e0575
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-06-21 08:59:53 -0300

    [UPT][TASK-????] Component about

---
+++ src/app/salary/component/about/about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-salary-about',
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.scss'
+})
+export class SalaryAboutComponent {}
