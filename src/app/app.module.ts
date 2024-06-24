001d3c509c146d784de0223e96c3d3a0bba5ada8
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-06-24 13:24:41 -0300

    [UPT][TASK-????] Adicionando a lib de "ngx-mask"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { IConfig, provideEnvironmentNgxMask } from 'ngx-mask';
+
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { SalaryModule } from './salary/module/salary.module';
+
+const maskConfig: Partial<IConfig> = {
+    validation: false,
+};
 
 @NgModule({
     declarations: [
@@ -25,7 +31,8 @@ import { SalaryModule } from './salary/module/salary.module';
         SalaryModule,
     ],
     providers: [
-        provideAnimationsAsync()
+        provideAnimationsAsync(),
+        provideEnvironmentNgxMask(maskConfig),
     ],
     bootstrap: [AppComponent]
 })
